3d5298dc2ef39a111e6293dced6fef2cbc6b1528
Agussantos84704 <124318393+Agussantos84704@users.noreply.github.com>
2023-08-01 22:36:10 -0300

    Add files via upload

---
+++ ejercicio/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { CartaPresentacion } from './components/CartaPresentacion';
+
+
+function App() {
+  return (
+    <body>
+    <h1>Hola Mundo con React!</h1> 
+    <CartaPresentacion>
+      
+    </CartaPresentacion>
+    </body>
+    
+  );
+}
+
+export default App;
---
+++ ejercicio/src/components/CartaPresentacion.jsx
@@ -0,0 +1,14 @@
+const CartaPresentacion = () => {
+    return (
+      <body>
+
+       <header> <h1>Agustin Santos Grecco</h1></header>
+      <div><span >Tengo conocimientos de HTML, CSS y JavaScript</span>
+      
+      <aside> Mis aspiarciones actualmente, son aprender react y empezar Backend!</aside>
+      </div>
+      </body>
+      )
+  }
+  
+  export {CartaPresentacion};
